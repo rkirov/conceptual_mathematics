defe09f0548b1e3be16160cb3df763572fd3d18f
Rado Kirov <rkirov@gmail.com>
2026-07-02 21:59:37 -0700

    Finish Session04

--- ConceptualMathematics/Sorried/Session04.lean
+++ ConceptualMathematics/Sorried/Session04.lean
@@ -7,77 +7,41 @@ local notation:80 g " ⊚ " f:80 => CategoryStruct.comp f g
 Exercise 4.1 (p. 66)
 -/
 structure AlgebraicObj where
-  t : Type
-  carrier : Set t
-  oper : t → t → t
-  oper_mem {a b} : a ∈ carrier → b ∈ carrier → oper a b ∈ carrier
+  α : Type
+  op : α → α → α
 
 instance : Category AlgebraicObj where
-  Hom X Y := {
-    f : X.t ⟶ Y.t //
-        (∀ x ∈ X.carrier, f x ∈ Y.carrier) -- maps to codomain
-        ∧ (∀ x₁ ∈ X.carrier, -- respects combining-rules
-             ∀ x₂ ∈ X.carrier, f (X.oper x₁ x₂) = Y.oper (f x₁) (f x₂))
-  }
-  id X := ⟨
-    𝟙 X.t,
-    by
-      constructor
-      · intro _ hx
-        exact hx
-      · intros
-        rfl
-  ⟩
-  comp := by
-    rintro _ _ _ ⟨f, hf⟩ ⟨g, hg⟩
-    exact ⟨
-      g ⊚ f,
-      by
-        obtain ⟨hf_mtc, hf_comb⟩ := hf
-        obtain ⟨hg_mtc, hg_comb⟩ := hg
-        constructor
-        · intro x hx
-          exact hg_mtc (f x) (hf_mtc x hx)
-        · intro x₁ hx₁ x₂ hx₂
-          dsimp
-          have h₁ := hf_comb x₁ hx₁ x₂ hx₂
-          rw [h₁]
-          have h₂ :=
-            hg_comb (f x₁) (hf_mtc x₁ hx₁) (f x₂) (hf_mtc x₂ hx₂)
-          rw [h₂]
-    ⟩
+  Hom X Y := { f : X.α → Y.α // ∀ x y : X.α, f (X.op x y) = Y.op (f x) (f y) }
+  id _ := ⟨fun x => x, by grind⟩
+  comp := fun f g => ⟨fun x => g.val (f.val x), by grind⟩
 
 def RealAdd : AlgebraicObj := {
-  t := ℝ
-  carrier := Set.univ
-  oper := (· + ·)
-  oper_mem := fun _ _ ↦ Set.mem_univ _
+  α := ℝ
+  op := (· + ·)
 }
 
 namespace Ex4_1
 
-def d : RealAdd ⟶ RealAdd := ⟨
-  fun (x : ℝ) ↦ 2 * x,
-  by
-    constructor
-    · exact fun _ _ ↦ Set.mem_univ _
-    · intros
-      dsimp [RealAdd]
-      ring
+def d : RealAdd ⟶ RealAdd := ⟨fun (x : ℝ) ↦ 2 * x, by
+  intro x y
+  simp [RealAdd]
+  ring
 ⟩
 
-noncomputable def h : RealAdd ⟶ RealAdd := ⟨
-  fun (x : ℝ) ↦ x / 2,
-  by
-    constructor
-    · exact fun _ _ ↦ Set.mem_univ _
-    · intros
-      dsimp [RealAdd]
-      ring
+noncomputable def h : RealAdd ⟶ RealAdd := ⟨fun (x : ℝ) ↦ x / 2, by
+  intro x y
+  simp [RealAdd]
+  ring
 ⟩
 
-example : IsIso d :=
-  sorry
+example : IsIso d := by
+  use h
+  constructor
+  · simp [h, d, RealAdd, CategoryStruct.id]
+    simp [CategoryStruct.comp]
+  · simp [h, d, RealAdd, CategoryStruct.id]
+    simp [CategoryStruct.comp]
+    grind
 
 end Ex4_1
 
@@ -88,6 +52,7 @@ namespace Ex4_2
 
 inductive Parity
   | odd | even
+  deriving Fintype
 
 def add : Parity → Parity → Parity
   | Parity.odd, Parity.odd => Parity.even
@@ -100,6 +65,7 @@ instance : Add Parity where
 
 inductive Sign
   | pos | neg
+  deriving Fintype
 
 def mul : Sign → Sign → Sign
   | Sign.pos, Sign.pos => Sign.pos
@@ -111,76 +77,137 @@ instance : Mul Sign where
   mul := mul
 
 def parityAdd : AlgebraicObj := {
-  t := Parity
-  carrier := Set.univ
-  oper := (· + ·)
-  oper_mem := fun _ _ ↦ Set.mem_univ _
+  α := Parity
+  op := (· + ·)
 }
 
 def signMul : AlgebraicObj := {
-  t := Sign
-  carrier := Set.univ
-  oper := (· * ·)
-  oper_mem := fun _ _ ↦ Set.mem_univ _
+  α := Sign
+  op := (· * ·)
 }
 
 def f : parityAdd ⟶ signMul :=
-  sorry
+  ⟨fun p ↦
+    match p with
+    | Parity.odd => Sign.neg
+    | Parity.even => Sign.pos,
+  by
+    intro x y
+    cases x <;> cases y <;> rfl⟩
 
 def finv : signMul ⟶ parityAdd :=
-  sorry
+  ⟨fun s ↦
+    match s with
+    | Sign.neg => Parity.odd
+    | Sign.pos => Parity.even,
+  by
+    intro x y
+    cases x <;> cases y <;> rfl⟩
 
-example : IsIso f :=
-  sorry
+example : IsIso f := by
+  use finv
+  constructor
+  · simp [f, finv, parityAdd, signMul, CategoryStruct.comp, CategoryStruct.id]
+    grind
+  · simp [f, finv, parityAdd, signMul, CategoryStruct.comp, CategoryStruct.id]
+    grind
 
 end Ex4_2
 
 /-!
 Exercise 4.3 (p. 66)
 -/
-example {p : ℝ ⟶ ℝ} (hp : ∀ x : ℝ, p x = x + 1)
-    : ¬(∀ a b : ℝ, p (a + b) = p a + p b) :=
-  sorry
+-- a) can't be made to AlgebraicObj
+def p : ℝ → ℝ := fun x => x + 1
+example : ¬(∀ a b : ℝ, p (a + b) = p a + p b) := by
+  push Not
+  use 1, 1
+  simp [p]
 
-example {sq : ℝ ⟶ ℝ} (hsq : ∀ x : ℝ, sq x = x ^ 2)
-    : ¬(∃ r : ℝ → ℝ, (∀ x : ℝ, r (sq x) = x)) :=
-  sorry
+-- b)
+def sq := fun (x : ℝ) => x ^ 2
+def RealMul : AlgebraicObj := {
+  α := ℝ
+  op := (· * ·)
+}
+def sq' : RealMul ⟶ RealMul := ⟨sq, by
+  intro x y
+  simp [RealMul, sq]
+  ring
+⟩
 
+example : ¬ IsIso sq' := by
+  intro h
+  obtain ⟨g, hg, _⟩ := h
+  simp only [RealMul, CategoryStruct.comp, sq', sq, CategoryStruct.id] at hg
+  have hf := Subtype.ext_iff.mp hg
+  have h1 := congr_fun hf (-1)
+  have h2 := congr_fun hf 1
+  simp only [even_two, Even.neg_pow, one_pow] at h1 h2
+  rw [h1] at h2
+  norm_num at h2
+
+-- c)
 open NNReal in
-example {sq : ℝ ⟶ ℝ≥0} (hsq : ∀ x : ℝ, sq x = x ^ 2)
-    : ¬(∃ r : ℝ → ℝ, (∀ x : ℝ, r (sq x) = x)) :=
-  sorry
+def PosRealMul : AlgebraicObj := {
+  α := ℝ≥0
+  op := fun x y => ⟨x.val * y.val, mul_nonneg x.property y.property⟩
+}
 
+def sq'' : RealMul ⟶ PosRealMul := ⟨fun (x : ℝ) ↦ ⟨x ^ 2, by positivity⟩, by
+  intro x y
+  simp only [RealMul, PosRealMul]
+  apply Subtype.ext
+  ring
+⟩
+
+example : ¬ IsIso sq'' := by
+  intro h
+  obtain ⟨g, hg, hg'⟩ := h
+  simp only [PosRealMul, CategoryStruct.comp, sq'', CategoryStruct.id] at hg hg'
+  have hf := Subtype.ext_iff.mp hg
+  simp only at hf
+  have h1 := congr_fun hf (1 : ℝ)
+  have h2 := congr_fun hf (-1 : ℝ)
+  norm_num at h1 h2
+  rw [h1] at h2
+  change (1 : ℝ) = -1 at h2
+  norm_num at h2
+
+-- d)
 namespace Ex4_3_d
 
 def m : RealAdd ⟶ RealAdd := ⟨
-  fun (x : ℝ) ↦ -x,
-  by
-    constructor
-    · exact fun _ _ ↦ Set.mem_univ _
-    · intros
-      dsimp [RealAdd]
-      ring
+  fun (x : ℝ) ↦ -x, by
+    intros
+    dsimp [RealAdd]
+    ring
 ⟩
 
-def minv := m
-
-example : IsIso m :=
-  sorry
+example : IsIso m := by
+  use m
+  constructor <;> simp [m, RealAdd, CategoryStruct.comp, CategoryStruct.id]
 
 end Ex4_3_d
 
-example {m : ℝ ⟶ ℝ} (hm : ∀ x : ℝ, m x = -x)
-    : ¬(∀ a b : ℝ, m (a * b) = m a * m b) :=
-  sorry
+-- e)
 
+def m : ℝ → ℝ := fun (x : ℝ) => -x
+example : ¬(∀ a b : ℝ, m (a * b) = m a * m b) := by
+  push Not
+  use -1, -1
+  simp [m]
+  norm_num
+
+-- f)
 namespace Ex4_3_f
 
-abbrev ℝpos := { x : ℝ // x > 0 }
-
-example {c : ℝ → ℝpos} (hc : ∀ x : ℝ, c x = x ^ 3)
-    : ∃ x : ℝ, ¬(∃ y : ℝpos, y.val = c x) :=
-  sorry
+-- the domain is not correct
+def c := fun (x : ℝ) => x ^ 3
+example : ∃ x : ℝ, ¬ c x > 0 := by
+  use -1
+  simp [c]
+  norm_num
 
 end Ex4_3_f
 
